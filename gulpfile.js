62fe3e54209c082e3453abc75a0428e56b5751e7
Josh Beveridge <josh.beveridge@outlook.com>
2020-11-16 11:39:27 -0500

    Updates to core, migrates to the new dist structure.

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,13 @@
 
 // Requirements
 const { series } = require('gulp');
-const importInit = require("./tasks/init");
 const importDev = require("./tasks/dev");
 const importBuild = require("./tasks/build");
 
 // Exports
 
-    // gulp init --c=name
-    exports.init = series(importInit.exportInit);
+  // gulp dev
+  exports.dev = series(importDev.exportDev);
 
-    // gulp dev
-    exports.dev = series(importDev.exportDev);
-
-    // gulp build
-    exports.build = series(importBuild.exportBuild);
+  // gulp build
+  exports.build = series(importBuild.exportBuild);
