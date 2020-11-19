069480e74de4c9078c644506c90d6afdd7310c07
Josh Beveridge <josh.beveridge@outlook.com>
2020-11-19 14:37:40 -0500

    Updates CHANGELOG with null dates for past releases.

---
+++ dist/import/latest/scripts/module.js
@@ -0,0 +1,9 @@
+// Hydrogen / Component / Module JS
+
+// This file is designed to be the location in which all scripting for the component's functionality exists. This file should export all relevant functionality so that it can be imported by Hydrogen and other systems.
+
+// Development Notes:
+// - please ensure all functions are defined with "" at the end of their name.
+// - please ensure all references to the parent component's selector also include "" (e.g. [data-h2-accordion])
+//   This is so that when the component is built, it produces a version-locked set of code that can me manually imported to override newer versions.
+// - please ensure that when event listeners are added to a trigger, that the script is checking for the system variable (see an example below).
