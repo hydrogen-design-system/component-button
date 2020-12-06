0b8ad7f065ecd90fc10b582135c56e906011646d
Josh Beveridge <josh.beveridge@outlook.com>
2020-12-06 12:23:16 -0500

    Updates the core, changing the way color and shades are referenced.

--- dist/import/0012/scripts/module.babel.js
+++ dist/import/0012/scripts/module.babel.js
@@ -1,8 +1,8 @@
 // Hydrogen / Component / Module JS
 // This file is designed to be the location in which all scripting for the component's functionality exists. This file should export all relevant functionality so that it can be imported by Hydrogen and other systems.
 // Development Notes:
-// - please ensure all functions are defined with "0011" at the end of their name.
-// - please ensure all references to the parent component's selector also include "0011" (e.g. [data-h2-accordion0011])
+// - please ensure all functions are defined with "0012" at the end of their name.
+// - please ensure all references to the parent component's selector also include "0012" (e.g. [data-h2-accordion0012])
 //   This is so that when the component is built, it produces a version-locked set of code that can me manually imported to override newer versions.
 // - please ensure that when event listeners are added to a trigger, that the script is checking for the system variable (see an example below).
 "use strict";
